04480adc64727c096bd7610a7d35cad28e66d87e
SwimmingTiger <hu60.cn@gmail.com>
2018-06-27 22:26:09 +0800

    change console codepage to UTF-8

--- dsn_service/dsn_service/src/main.cpp
+++ dsn_service/dsn_service/src/main.cpp
@@ -2,7 +2,7 @@
 * “龙裔自然对话”服务端主程序
 */
 
-#include <conio.h>
+#include <Windows.h>
 #include "DSNService.h"
 #include "aixlog.hpp"
 #include "functional"
@@ -12,6 +12,9 @@ const char *DSN_LOG_FILE = "./DragonbornSpeaksNaturally.log";
 int main(int argc, char* argv[])
 {
 	AixLog::Log::init<AixLog::SinkFile>(AixLog::Severity::trace, AixLog::Type::all, DSN_LOG_FILE);
+
+	SetConsoleCP(CP_UTF8);
+	SetConsoleOutputCP(CP_UTF8);
 
 	DSNService dsnService;
 	dsnService.start();
